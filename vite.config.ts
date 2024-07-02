6b599824b65444e6fd8b3b676c5b4bd8c608ef33
方胜 <2030192443@qq.com>
2024-07-02 23:53:42 +0800

    更新 Progress 组件和调整上传相关逻辑
    - 升级 Progress 组件至 2.0.2 版本，适配新的 API 和样式调整。
    - 修复上传功能中文件重复添加的问题，优化文件列表的显示逻辑。
    - 增加上传文件时的权限验证，确保只有授权用户可以进行文件上传操作。

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
+  base:"/admin/",
+  
   plugins: [
     vue(),
     AutoImport({
